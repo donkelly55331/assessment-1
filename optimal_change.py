1f6e47c5bfa72cce87ec185e76f1193337374933
donkelly55331 <donkelly55331@gmail.com>
2021-06-05 16:56:53 -0500

    Don's completed assessment 1

--- optimal_change.py
+++ optimal_change.py
@@ -1,1 +1,78 @@
-# Write your solution here!
+# Class Vendor
+
+# Give optimal change for item's cost from amount paid
+# Change values:
+#   Bills 100, 50, 20, 10, 5, 1
+#   Coins 25, 10, 5, 1# Write your solution here!
+
+# Given item cost and amount paid
+# set change due = amount paid - item cost
+# for largest to smallest denomination (bill or coin)
+#   count is int of change_due / denomination
+#       reduce change due by count * denomination
+#       # of items in change at denomination index is count
+
+import math
+
+
+class Vendor():
+
+    def __init__(self, name):
+        self.name = name
+
+    def make_change(self, item_cost, amount_paid):
+        denominations = [10000, 5000, 2000, 1000, 500, 100, 25, 10, 5, 1]
+        change = []
+        due = amount_paid * 100 - item_cost * 100
+
+        for denomination in denominations:
+            count = math.floor(due/denomination)
+            due -= count * denomination
+            change.append(count)
+
+        return change
+
+    
+    def plural(self, count, item):
+        if count < 2:
+            return item
+        elif item == 'penny':
+            return 'pennies'
+        else:
+            return item + 's'
+
+    def say_change(self, item_cost, amount_paid):
+
+        if item_cost == amount_paid:
+            return f"There is no change due for an item that costs ${item_cost} with an amount paid of ${amount_paid}."
+
+        if item_cost > amount_paid:
+            return f"There is a balance due of ${(item_cost - amount_paid):.2f}."
+
+        items = ['$100 bill', '$50 bill', '$20 bill', '$10 bill', '$5 bill', '$1 bill', 'quarter', 'dime', 'nickel', 'penny']
+
+        change = self.make_change(item_cost, amount_paid)
+
+        string = f"The optimal change for an item that costs ${item_cost} with an amount paid of ${amount_paid} is "
+        
+        # Need 'and'? Where?
+        num_items = 0
+        last_index = -1
+        for i in reversed(range(10)):
+            if change[i] > 0:
+                num_items += i 
+                if last_index == -1:
+                    last_index = i 
+
+        for index, count in enumerate(change):
+            if count > 0:
+
+                if index == last_index and num_items > 1:
+                    string += 'and '
+
+                string += f"{count} {self.plural(count, items[index])}"
+
+                if index < last_index:
+                    string += ', '
+
+        return string + '.'
--- optimal_change_spec.py
+++ optimal_change_spec.py
@@ -1,1 +1,46 @@
 # Write your unit tests here!
+import unittest
+from optimal_change import *
+
+# Test computer for vending machine
+# Give optimal change for item's cost from amount paid
+# Change values:
+#   Bills 100, 50, 20, 10, 5, 1
+#   Coins 25, 10, 5, 1
+
+class ValidateOptimalChange(unittest.TestCase):
+
+    def test_change_1(self):
+        vendor = Vendor('Lollipop Machine')
+        self.assertEqual(vendor.make_change(62.13, 100), [0, 0,1,1,1,2,3,1,0,2])
+
+    def test_change_2(self):
+        vendor = Vendor('Lollipop Machine')
+        self.assertEqual(vendor.make_change(31.51, 50), [0,0,0,1,1,3,1,2,0,4])
+
+    def test_change_3(self):
+        vendor = Vendor('Lollipop Machine')
+        self.assertEqual(vendor.say_change(62.13, 100), "The optimal change for an item that costs $62.13 with an amount paid of $100 is 1 $20 bill, 1 $10 bill, 1 $5 bill, 2 $1 bills, 3 quarters, 1 dime, and 2 pennies.")
+
+    def test_change_4(self):
+        vendor = Vendor('Lollipop Machine')
+        self.assertEqual(vendor.say_change(31.51, 50), "The optimal change for an item that costs $31.51 with an amount paid of $50 is 1 $10 bill, 1 $5 bill, 3 $1 bills, 1 quarter, 2 dimes, and 4 pennies.")
+
+    def test_change_5(self):
+        vendor = Vendor('Lollipop Machine')
+        self.assertEqual(vendor.make_change(62.87, 100.12), [0,0,1,1,1,2,1,0,0,0])
+
+    def test_change_6(self):
+        vendor = Vendor('Lollipop Machine')
+        self.assertEqual(vendor.say_change(62.87, 100.12), "The optimal change for an item that costs $62.87 with an amount paid of $100.12 is 1 $20 bill, 1 $10 bill, 1 $5 bill, 2 $1 bills, and 1 quarter.") 
+
+    def test_change_7(self):
+        vendor = Vendor('Lollipop Machine')
+        self.assertEqual(vendor.say_change(12.34, 12.34), "There is no change due for an item that costs $12.34 with an amount paid of $12.34.")
+
+    def test_change_8(self):
+        vendor = Vendor('Lollipop Machine')
+        self.assertEqual(vendor.say_change(52.34, 42.34), "There is a balance due of $10.00.")
+
+if __name__ == '__main__':
+    unittest.main()
